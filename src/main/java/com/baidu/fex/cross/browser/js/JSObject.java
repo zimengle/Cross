bd6aa0179423d1fa243c604915bb17e7c8730f9b
pengzhanlee <live@jsmix.com>
2013-12-12 01:24:22 +0800

    reflection

--- src/main/java/com/baidu/fex/cross/browser/js/JSObject.java
+++ src/main/java/com/baidu/fex/cross/browser/js/JSObject.java
@@ -1,5 +1,8 @@
 package com.baidu.fex.cross.browser.js;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import android.os.Handler;
 
 public class JSObject {
@@ -9,6 +12,8 @@ public class JSObject {
 	private Handler handler;
 
 	private float pageRatio;
+	
+	private String packageName = JSObject.class.getPackage().getName();
 
 	public JSObject(Handler handler) {
 		this.handler = handler;
@@ -27,7 +32,36 @@ public class JSObject {
 		contactInputJSObject.setPageRatio(pageRatio);
 		return contactInputJSObject;
 	}
-
 	
+	public InteractableJSObj getSlider() {
+		InteractableJSObj sliderJSObject = new SliderJSObject(handler);
+		sliderJSObject.setPageRatio(pageRatio);
+		return sliderJSObject;
+	}
+	
+	public InteractableJSObj getComponent(String componentName){
+		String componentClassName = packageName+"."+componentName+"JSObject";
+		InteractableJSObj instance = null;
+		try {
+			Class<? extends InteractableJSObj> interactableJSObjClass = Class.forName(componentClassName).asSubclass(InteractableJSObj.class);
+			Constructor<? extends InteractableJSObj> constructor = interactableJSObjClass.getConstructor(Handler.class);
+			instance = constructor.newInstance(handler);
+			instance.setPageRatio(pageRatio);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		
+		return instance;
+	}
 	
 }
